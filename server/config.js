aad78ad3e3931276ea383dc599881fc344c7d589
hamadsafdar <hamadsafdar57@gmail.com>
2020-04-05 15:15:35 +0500

    Config.js

---
+++ server/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    db: {
+        USER: 'mybitcoiner',
+        PWD: '123456789db',
+    },
+    PORT: 3004,
+    secret: {
+        USER: 'mango.people',
+        ADMIN: 'webmaster',
+    },
+    role: {
+        ADMIN_ROLE: 'WEB_MASTER',
+        USER_ROLE: 'WEB_USER'
+    },
+    email: {
+        USER: 'moviecon3327@gmail.com',
+        PASS: '03105593105'
+    }
+}
