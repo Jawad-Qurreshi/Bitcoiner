dca1a23b85328906cd961851c9585b21de98fd51
hamadsafdar <hamadsafdar57@gmail.com>
2020-04-13 18:16:31 +0500

    OK

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,6 +14,7 @@ const config = require('../config');
 const mailer = require('../mailer');
 //Check User middleware
 const user = require('../middlewares/user-middlware');
+const admin = require('../middlewares/admin-middleware');
 //Controllers
 const adminController = require('../controllers/admin-controllers/admin-controller');
 const buyController = require('../controllers/user-controllers/user-buy-controller');
@@ -34,190 +35,189 @@ mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, fun
 });
 
 ///////////////////////Password//////////////////////
-
-router.post('/user/recovery/password', (req, res) => {
-    const to = req.body.email;
-    mailer.sendPasswordRecoveryEmail(to);
-    res.status(200).json({
-        isSuccess: true,
-        message: 'MAIL_SENT'
-    });
-});
-
-///////////////////////////////////BIT API/////////////////////////////////////
-const binance = require('node-binance-api')().options({
-    APIKEY: 'https://api.binance.com/api/v3/ticker/price',
-    useServerTime: true // If you get timestamp errors, synchronize to server time at startup
-});
-//Getting bitcoin price
-router.get('/bitapi', function (req, res) {
-    binance.prices('BTCUSDT', (error, ticker) => {
-        res.send({
-            ticker: ticker
+{
+    router.post('/user/recovery/password', (req, res) => {
+        const to = req.body.email;
+        mailer.sendPasswordRecoveryEmail(to);
+        res.status(200).json({
+            isSuccess: true,
+            message: 'MAIL_SENT'
         });
     });
-});
-//Getting ethereium price
-router.get('/ethapi', user.checAuth, function (req, res) {
-    binance.prices('ETHUSDT', (error, ticker) => {
-        res.json({
-            ticker: ticker
+
+    ///////////////////////////////////BIT API/////////////////////////////////////
+    const binance = require('node-binance-api')().options({
+        APIKEY: 'https://api.binance.com/api/v3/ticker/price',
+        useServerTime: true // If you get timestamp errors, synchronize to server time at startup
+    });
+    //Getting bitcoin price
+    router.get('/bitapi', function (req, res) {
+        binance.prices('BTCUSDT', (error, ticker) => {
+            res.send({
+                ticker: ticker
+            });
         });
     });
-});
-////////////////////////////Buyers////////////////////////////////
-router.post('/buyer/add', user.checAuth, (req, res) => {
-    const body = req.body;
-    const limit = body.limit;
-    const clientId = req.decoded.userid;
-    limit.minimum = parseFloat(limit.minimum);
-    limit.maximum = parseFloat(limit.maximum);
-
-    const buyPost = new TradePost({
-        name: body.name,
-        cryptoType: body.cryptoType,
-        price: parseFloat(body.price),
-        description: body.description,
-        limit: limit,
-        amount: parseFloat(body.amount),
-        postType: 'Buy',
-        clientId: clientId
+    //Getting ethereium price
+    router.get('/ethapi', user.checAuth, function (req, res) {
+        binance.prices('ETHUSDT', (error, ticker) => {
+            res.json({
+                ticker: ticker
+            });
+        });
     });
+    ////////////////////////////Buyers////////////////////////////////
+    router.post('/buyer/add', user.checAuth, (req, res) => {
+        const body = req.body;
+        const limit = body.limit;
+        const clientId = req.decoded.userid;
+        limit.minimum = parseFloat(limit.minimum);
+        limit.maximum = parseFloat(limit.maximum);
 
-    buyPost.save()
-        .then(stored => {
-            res.status().json({
-                isSuccess: true,
-                message: 'SELL_POSTED'
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                isSuccess: false,
-                message: 'INTERNAL_ERROR'
-            });
+        const buyPost = new TradePost({
+            name: body.name,
+            cryptoType: body.cryptoType,
+            price: parseFloat(body.price),
+            description: body.description,
+            limit: limit,
+            amount: parseFloat(body.amount),
+            postType: 'Buy',
+            clientId: clientId
         });
-});
-router.get('/buyer/all', user.checAuth, function (req, res) {
-    TradePost.find({ postType: 'buy' })
-        .exec()
-        .then(result => {
 
-            res.status(200).json({
-                result: result,
-                isSuccess: true
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: err.message,
-                isSuccess: false
-            })
-        });
-});
-router.delete('/buyer/:id', user.checAuth, function (req, res) {
-    TradePost.findByIdAndRemove(req.params.id, function (err, deletedBuyer) {
-        if (err) {
-            res.send("Error deleting buyer")
-        } else {
-            res.json(deletedBuyer);
-        }
-    })
-});
-router.post('/confirm/sell', user.checAuth, buyController.confirmSell);
-/////////////////Client WIthdraw//////////
-
-router.post('/request/withdraw', user.checAuth, user.isVerified, userController.requestWithDraw);
-router.get('/request/withdraw', user.checAuth, user.isVerified, userController.getWithdrawRequests);
-
-
-////////////////////////////Sellers//////////////////////////////////
-router.post('/seller/add', user.checAuth, async (req, res) => {
-    const body = req.body;
-    const clientId = req.decoded.userid;
-    const limit = req.body.limit;
-    limit.minimum = parseFloat(limit.minimum);
-    limit.maximum = parseFloat(limit.maximum);
-    const sellPost = new TradePost({
-        name: body.name,
-        limit: limit,
-        amount: parseFloat(body.amount),
-        cryptoType: body.cryptoType,
-        price: parseFloat(body.price),
-        description: body.description,
-        clientId: clientId,
-        type: 'Sell'
-    });
-    sellPost.save()
-        .then(stored => {
-            res.status(200).json({
-                isSuccess: true,
-                message: 'SELL_POSTED'
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                isSuccess: false,
-                message: 'INTERNAL_ERROR'
-            });
-        });
-});
-router.get('/seller/all', user.checAuth, function (req, res) {
-    TradePost.find({ postType: 'Sell' })
-        .exec(function (err, sellPost) {
-            if (err) {
-                console.log('Error while retrieving All seller in api.js');
-            } else {
-                res.json({
-                    result: sellPost
+        buyPost.save()
+            .then(stored => {
+                res.status().json({
+                    isSuccess: true,
+                    message: 'SELL_POSTED'
                 });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    isSuccess: false,
+                    message: 'INTERNAL_ERROR'
+                });
+            });
+    });
+    router.get('/buyer/all', user.checAuth, function (req, res) {
+        TradePost.find({ postType: 'buy' })
+            .exec()
+            .then(result => {
+
+                res.status(200).json({
+                    result: result,
+                    isSuccess: true
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message,
+                    isSuccess: false
+                })
+            });
+    });
+    router.delete('/buyer/:id', user.checAuth, function (req, res) {
+        TradePost.findByIdAndRemove(req.params.id, function (err, deletedBuyer) {
+            if (err) {
+                res.send("Error deleting buyer")
+            } else {
+                res.json(deletedBuyer);
             }
+        })
+    });
+    router.post('/confirm/sell', user.checAuth, buyController.confirmSell);
+    /////////////////Client WIthdraw//////////
+
+    router.post('/request/withdraw', user.checAuth, user.isVerified, userController.requestWithDraw);
+    router.get('/request/withdraw', user.checAuth, user.isVerified, userController.getWithdrawRequests);
+
+
+    ////////////////////////////Sellers//////////////////////////////////
+    router.post('/seller/add', user.checAuth, async (req, res) => {
+        const body = req.body;
+        const clientId = req.decoded.userid;
+        const limit = req.body.limit;
+        limit.minimum = parseFloat(limit.minimum);
+        limit.maximum = parseFloat(limit.maximum);
+        const sellPost = new TradePost({
+            name: body.name,
+            limit: limit,
+            amount: parseFloat(body.amount),
+            cryptoType: body.cryptoType,
+            price: parseFloat(body.price),
+            description: body.description,
+            clientId: clientId,
+            type: 'Sell'
         });
-})
-router.delete('/seller/:id', user.checAuth, function (req, res) {
-    TradePost.findByIdAndRemove(req.params.id, function (err, deletedSeller) {
-        if (err) {
-            res.send("Error deleting client")
-        } else {
-            res.json(deletedSeller);
-        }
+        sellPost.save()
+            .then(stored => {
+                res.status(200).json({
+                    isSuccess: true,
+                    message: 'SELL_POSTED'
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    isSuccess: false,
+                    message: 'INTERNAL_ERROR'
+                });
+            });
+    });
+    router.get('/seller/all', user.checAuth, function (req, res) {
+        TradePost.find({ postType: 'Sell' })
+            .exec(function (err, sellPost) {
+                if (err) {
+                    console.log('Error while retrieving All seller in api.js');
+                } else {
+                    res.json({
+                        result: sellPost
+                    });
+                }
+            });
     })
-})
-router.post('/confirm/buy', user.checAuth, sellController.confirmBuy);
-////////////////////Client's Requests routes/////////////
-router.post('/request/add', user.checAuth, userRequestController.addRequest);
-router.delete('/request/:requestId', user.checAuth, userRequestController.deleteRequest);
-router.get('/request/approved', user.checAuth, userRequestController.getApprovedRequests);
-router.get('/request/pending', user.checAuth, userRequestController.getPendingRequests);
-///////////////////////////login   Signup////////////////////////////////////
-router.post('/signup', userController.signUp);
-router.post('/login', userController.logIn);
-router.post('/user/update', user.checAuth, userController.updateUser);
+    router.delete('/seller/:id', user.checAuth, function (req, res) {
+        TradePost.findByIdAndRemove(req.params.id, function (err, deletedSeller) {
+            if (err) {
+                res.send("Error deleting client")
+            } else {
+                res.json(deletedSeller);
+            }
+        })
+    })
+    router.post('/confirm/buy', user.checAuth, sellController.confirmBuy);
+    ////////////////////Client's Requests routes/////////////
+    router.post('/request/add', user.checAuth, userRequestController.addRequest);
+    router.delete('/request/:requestId', user.checAuth, userRequestController.deleteRequest);
+    router.get('/request/approved', user.checAuth, userRequestController.getApprovedRequests);
+    router.get('/request/pending', user.checAuth, userRequestController.getPendingRequests);
+    ///////////////////////////login   Signup////////////////////////////////////
+    router.post('/signup', userController.signUp);
+    router.post('/login', userController.logIn);
+    router.post('/user/update', user.checAuth, userController.updateUser);
 
-////////////////////////////////Clients///////////////////////////////////////////
+    ////////////////////////////////Clients///////////////////////////////////////////
+    router.get('/client', user.checAuth, userController.getClient);
+    //////////////////////////// Buy/Sell Posts of Client/////////////////////
+    router.get('/post/all', user.checAuth, userController.getClientPosts);
+    router.delete('/post/:postId', user.checAuth, userController.deletePost);
 
-router.get('/client', user.checAuth, userController.getClient);
-
-
-//////////////////////////// Buy/Sell Posts of Client/////////////////////
-router.get('/post/all', user.checAuth, userController.getClientPosts);
-router.delete('/post/:postId', user.checAuth, userController.deletePost);
+}
 ////////////////////////////ADMIN/////////////////////////
 {
-    router.get('/admin/request/withdraw/all', adminController.getWithdrawRequests);
+    router.get('/admin/request/withdraw/all', admin.checkAdminAuth, adminController.getWithdrawRequests);
     router.post('/admin/create', adminController.createAdmin);
     router.post('/admin/authenticate', adminController.checkAdminAuth);
-    router.put('/admin/verify/user/:userId', adminController.verifyUser);
+    router.put('/admin/verify/user/:userId', admin.checkAdminAuth, adminController.verifyUser);
     ///////////////////////Currencey Address//////////////
-    router.post('/address/add', user.checAuth, adminController.addAddress);
-    router.get('/address/all', user.checAuth, adminController.getAllAddress);
-    router.get('/client/all', user.checAuth, adminController.getAllClients);
+    router.post('/address/add', admin.checkAdminAuth, adminController.addAddress);
+    router.get('/address/all', admin.checkAdminAuth, adminController.getAllAddress);
+    router.get('/client/all', admin.checkAdminAuth, adminController.getAllClients);
     //Admin's requests routes
     //This approves a request
-    router.put('/request/approve/:id', user.checAuth, adminController.approveRequest);
-
-    router.get('/admin/request/approved/all', adminController.getApprovedRequests);
-    router.get('/admin/request/pending/all', adminController.getPendingRequests);
-    router.put('/admin/verify/withdraw/:requestId', adminController.verifyWithdraw);
+    router.put('/request/approve/:id', admin.checkAdminAuth, adminController.approveRequest);
+    router.get('/admin/request/approved/all', admin.checkAdminAuth, adminController.getApprovedRequests);
+    router.get('/admin/request/pending/all', admin.checkAdminAuth, adminController.getPendingRequests);
+    router.put('/admin/verify/withdraw/:requestId', admin.checkAdminAuth, adminController.verifyWithdraw);
 }
+
 module.exports = router;
